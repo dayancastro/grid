427a9daec16389ac422699500a142b233cbe3ffe
Dayan <dcastro@terageosolucoes.com.br>
2018-06-08 21:57:57 -0300

    first commit.

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Popover } from 'antd';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      images: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/photos').then(response => {
+      this.setState({ images: response.data })
+    });
+  }
+
+  render() {
+    let { images } = this.state;
+    images = images.slice(0,50);
+    document.body.style.setProperty('--rowNum', images.length/5);
+
+    return (
+      <div className="grid-container">
+        {images.map(image => {
+          return (
+            <Popover content={<div><img src={image.url}/></div>} title={image.title} trigger="hover">
+            <div key={image.id} className="grid-item">
+              <img key={image.id} src={image.thumbnailUrl}/>
+            </div>
+            </Popover>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
